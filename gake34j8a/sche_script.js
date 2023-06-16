42537a1664f08b379c627d5da0f5a4afe0a9ced2
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2023-06-16 15:46:42 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -61,7 +61,7 @@ const musicGuid =[
     "musicname": "Inkigayo",
     "musicEP": "1187회",
     "musictime": "14:58KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">Baek Ye Bin, Park Rossi, SECRET NUMBER, fromis_9, NINE to SIX, THE NEW SIX (TNX), BOYNEXTDOOR, REN, LUN8, FANTASY BOYS, ATEEZ, DKB, VAV, CIX, P1Harmony, TAEYONG, ENHYPEN</font>`
   },  ////SP_GUIDE/////
   {
     "date": "Sun.",
