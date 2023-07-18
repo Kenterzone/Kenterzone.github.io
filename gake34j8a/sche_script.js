1cb283c4690d68f623938a98c08153fd4cf3028c
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2023-07-18 16:58:55 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -23,7 +23,7 @@ const musicGuid =[
     "musicname": "ShowChampion",
     "musicEP": "484회",
     "musictime": "18:04KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">ODD EYE CIRCLE, KISS OF LIFE, Lapillus, Queenz Eye, SWAN, NMIXX, ZEROBASEONE, 8TURN, LUN8, Nam Dong Hyun</font>`
   },
   {
     "mair": "nomal",
