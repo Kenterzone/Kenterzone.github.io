59cdf3bee861b2760055dcd2bda54fb8ee41eb30
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2023-08-29 17:42:14 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -23,7 +23,7 @@ const musicGuid =[
     "musicname": "ShowChampion",
     "musicEP": "489회",
     "musictime": "18:04KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">tripleS LOVElution, XG, ARTBEAT v, LIMELIGHT, SECRET NUMBER, EVERGLOW, YERIN, TIOT, LE'V, VANNER, DKB, xikers, n.SSign</font>`
   },
   {
     "mair": "nomal",
