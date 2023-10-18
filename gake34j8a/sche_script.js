7e5b2e9dc410511c8a4a93e75591c252d4f28451
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2023-10-18 17:05:43 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -32,7 +32,7 @@ const musicGuid =[
     "musicname": "M!CountDown",
     "musicEP": "819회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">YOUNG POSSE, Band UNI, LIGHTSUM, Rothy, Kep1er, IVE, Whee In, SUNMI, INI, Xdinary Heroes, n.SSign, ONF, ONEUS, YOUNITE, EVNNE, CRAVITY, KINGDOM, TOMORROW X TOGETHER, NCT 127</font>`
   },
 
   {
