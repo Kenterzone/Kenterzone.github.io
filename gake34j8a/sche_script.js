56522f33e231a3e29584499426f01a2f656b591e
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2023-11-07 18:10:31 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -23,7 +23,7 @@ const musicGuid =[
     "musicname": "ShowChampion",
     "musicEP": "499회",
     "musictime": "18:04KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">NiziU, Banduni, Billlie, LIGHTSUM, ILY:1, HeeJin(ARTMS), SORA, VIVIZ, NINE.i, XODIAC, KINGDOM, Moon Jong Up, Golden Child, WayV, GHOST9</font>`
   },
   {
     "mair": "nomal",
