cdb04f706a42f927ddc72d6c96e794b1c7feff6b
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2024-01-24 19:34:48 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -30,7 +30,7 @@ const musicGuid =[
     "date": "Thu.",
     "musiconoroff" : true,
     "musicname": "M!CountDown",
-    "musicEP": "825회",
+    "musicEP": "826회",
     "musictime": "18:08KST",
     "musicsetlist":`ShowGuest:<font color="#adadb8">KIM JAE HWAN, tripleS Aria, NMIXX, H1-KEY, YENA, ITZY, SEOLA(WJSN), SISTAR19, TWS, POW, BXB, EVNNE, JD1, CIX, AB6IX, HUI, SUPER JUNIOR-L.S.S.</font>`
   },
