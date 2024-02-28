3fc60c21caaac5d53960a0346904465e4c5cc52b
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2024-02-28 22:03:15 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -32,7 +32,7 @@ const musicGuid =[
     "musicname": "M!CountDown",
     "musicEP": "831회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">TRI.BE, LE SSERAFIM, Moon Byul, TWICE, NOMAD, AIMERS, n.SSign, TEN, The Wind, INI, A.C.E, CRAVITY, TAEYONG</font>`
   },
 
   {
